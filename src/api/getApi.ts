6a59618101d545af4991fa3e726a3c811557fb9c
Mikensonthomas142 <mikensonthomas2@gmail.com>
2025-12-28 20:29:19 -0300

    Commit

--- src/api/getApi.ts
+++ src/api/getApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  timeout: 1000,
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: Number(import.meta.env.VITE_API_TIMEOUT),
   headers: {'X-Custom-Header': 'foobar'}
 });
