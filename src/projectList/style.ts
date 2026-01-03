32e37f27c657d73e0835e7961a9ec36dfe2f8544
Mikensonthomas142 <mikensonthomas2@gmail.com>
2026-01-03 14:09:20 -0300

    Commit

--- src/projectList/style.ts
+++ src/projectList/style.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+
 export const Container = styled.div`
     width: 100%;
     color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export const Content = styled.div`
